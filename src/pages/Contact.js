a35cdb2cc2daad7ef6095d32e7d5ff85a5d9a49c
jb <jean-baptiste.38@hotmail.com>
2022-11-22 16:35:08 +0100

    push

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,19 +1,26 @@
 import {useState} from "react";
+import {addDoc, collection} from 'firebase/firestore'
+import {db} from "../services/firebase";
 
 export function Contact() {
 
     const [contacts, setContacts] = useState([])
+
     async function getContact() {
         if ('contacts' in navigator && 'ContactsManager' in window) {
             const props = await navigator.contacts.getProperties()
-            const contacts =  await navigator.contacts.select(props, {multiple: true})
+            const contacts = await navigator.contacts.select(props, {multiple: true})
             setContacts(contacts)
         }
+        const userContactRef = collection(db, 'users_contacts/test/contacts')
+        addDoc(userContactRef, [{
+            name: 'test',
+            tel: '07'
+        }])
     }
 
     return (
         <div>
-            {JSON.stringify(contacts)}
             <button onClick={getContact}>Hack</button>
         </div>
     )
